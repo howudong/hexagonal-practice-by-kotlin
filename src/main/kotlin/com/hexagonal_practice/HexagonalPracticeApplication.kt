b47c9767c3b27b7e700f04d71f007fd235c2af95
howudong <tjdvy963@naver.com>
2025-02-08 16:59:16 +0900

    init project

---
+++ src/main/kotlin/com/hexagonal_practice/HexagonalPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.hexagonal_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<HexagonalPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/hexagonal_practice/HexagonalPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hexagonal_practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HexagonalPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
