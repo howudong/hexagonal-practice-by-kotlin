367561bb41f44512f9b9f4402e5aeaf9e997c454
howudong <tjdvy963@naver.com>
2025-02-09 16:15:12 +0900

    feat : ActivityWindow 뼈대 완성

--- src/main/kotlin/com/hexagonal_practice/account/domain/ActivityWindow.kt
+++ src/main/kotlin/com/hexagonal_practice/account/domain/ActivityWindow.kt
@@ -1,4 +1,11 @@
 package com.hexagonal_practice.account.domain
 
-class ActivityWindow {
+import java.time.LocalDateTime
+
+class ActivityWindow(
+    private val activities: List<Activity>,
+) {
+    fun startTimestamp(): LocalDateTime? = activities.minOfOrNull { it.localDateTime }
+
+    fun endTimestamp(): LocalDateTime? = activities.maxOfOrNull { it.localDateTime }
 }
