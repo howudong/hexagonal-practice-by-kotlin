9f616742d9e96114e86ccae54c15141de5e6253e
howudong <tjdvy963@naver.com>
2025-02-10 21:47:54 +0900

    feat : 풍부한 도메인을 위한 첫걸음 : Account에 withdraw(출금) 로직 추가

--- src/main/kotlin/com/hexagonal_practice/account/domain/Account.kt
+++ src/main/kotlin/com/hexagonal_practice/account/domain/Account.kt
@@ -1,12 +1,40 @@
 package com.hexagonal_practice.account.domain
 
+import java.time.LocalDateTime
+
 class Account(
     private val id: AccountId? = null,
     private val baselineBalance: Money? = null,
-    private val activityWindow: ActivityWindow? = null,
+    private val activityWindow: ActivityWindow,
 ) {
     @JvmInline
     value class AccountId(
         private val value: Long,
     )
+
+    fun withdraw(
+        money: Money,
+        targetAccountId: AccountId,
+    ): Boolean {
+        // 비즈니스 유효성 검사
+        if (!mayWithdraw(money)) {
+            return false
+        }
+        val withdrawal =
+            Activity(
+                ownerAccountId = id!!,
+                sourceAccountId = id,
+                targetAccountId = targetAccountId,
+                localDateTime = LocalDateTime.now(),
+                money = money,
+            )
+
+        this.activityWindow.addActivity(withdrawal)
+        return true
+    }
+
+    private fun mayWithdraw(money: Money): Boolean {
+        // todo : 비즈니스 유효성 검사
+        return true
+    }
 }
--- src/main/kotlin/com/hexagonal_practice/account/domain/ActivityWindow.kt
+++ src/main/kotlin/com/hexagonal_practice/account/domain/ActivityWindow.kt
@@ -3,8 +3,14 @@ package com.hexagonal_practice.account.domain
 import java.time.LocalDateTime
 
 class ActivityWindow(
-    private val activities: List<Activity>,
+    private val activities: MutableList<Activity>,
 ) {
+    fun addActivity(activity: Activity) {
+        this.activities.add(activity)
+    }
+
+    fun getActivities(): List<Activity> = activities.toList()
+
     fun startTimestamp(): LocalDateTime? = activities.minOfOrNull { it.localDateTime }
 
     fun endTimestamp(): LocalDateTime? = activities.maxOfOrNull { it.localDateTime }
